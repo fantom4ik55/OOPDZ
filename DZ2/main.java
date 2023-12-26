4b56a87f0aa80c77a62b8c94cd40c65948d55807
Alex84 <fantom.rpg@mail.ru>
2023-12-26 22:28:31 +0300

    DZ2

--- DZ2/main.java
+++ DZ2/main.java
@@ -3,8 +3,8 @@ public class main {
         public static void main(String[] args) {
     
             Market market = new Market();
-            Human human1 = new Human("Дима");
-            Human human2 = new Human("Света");
+            Human human1 = new Human("Владимир Владимирович");
+            Human human2 = new Human("Джо Байдон");
     
             market.acceptToMarket(human2);
             market.acceptToMarket(human1);
